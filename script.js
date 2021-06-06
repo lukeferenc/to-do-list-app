b05276a24ef72e77824a108bc7b745d7e2d7437a
lukeferenc <luke.theduke.of.cornwall@gmail.com>
2021-06-06 18:13:03 +0200

    1.9 js added and edited

---
+++ script.js
@@ -0,0 +1,38 @@
+
+// Adding a new item to the list of items:
+function newItem(){
+    let li = $('<li></li>');
+    let inputValue = $('#input').val();
+    li.append(inputValue);
+      
+    if (inputValue === '') {
+      alert('You must write something!');
+    } else {
+      $('#list').append(li);
+        }
+  
+  
+    
+ // Crossing out an item from the list of items:  
+    function crossOut() {
+      li.toggleClass('strike');
+        }
+  
+    li.on('dblclick', function crossOut() {
+      li.toggleClass('strike');
+    });
+  
+  // Adding the delete button "X": 
+    let crossOutButton = $('<crossOutButton></crossOutButton>');
+    crossOutButton.append(document.createTextNode('X'));
+    li.append(crossOutButton);
+      
+    crossOutButton.on('click', deleteListItem);
+    function deleteListItem(){
+      li.addClass('delete')
+    }
+  
+  // Reordering the items:
+  
+    $('#list').sortable(); 
+  }
